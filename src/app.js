f7b1461542cfc2c07ea9c17aa39055e7d10c09a0
Shubham <shubhamsingh705010@gmail.com>
2024-01-13 13:57:15 +0530

    done for buiild

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const port = 3000;
 
+
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+import path from "path"
+
+module.exports = {
+  entry: './src/app.js',  // Update this path based on your entry file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  // Add other webpack configuration as needed...
+};
